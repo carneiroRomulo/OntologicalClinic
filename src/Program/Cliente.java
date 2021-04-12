cc2c02807564a2bb209230eaf194caa076a91b17
DESKTOP-LOBGSTI\Ananda <ananda.souza@aluno.ufop.edu.br>
2021-04-12 20:26:08 -0300

    Função abrir agenda 99% concluída

--- src/Program/Cliente.java
+++ src/Program/Cliente.java
@@ -10,8 +10,8 @@ public class Cliente extends Pessoa{
     public Cliente() {
     }
 
-    public Cliente(boolean pago, int diaPagamento, int mesPagamento, int anoPagamento, String nome, String sobrenome, String endereco, String email, String CPF, String RG, String telefone, String dataNascimento) {
-        super(nome, sobrenome, endereco, email, CPF, RG, telefone, dataNascimento);
+    public Cliente(boolean pago, int diaPagamento, int mesPagamento, int anoPagamento, String nome, String sobrenome, String endereco, String email, String CPF, String RG, String telefone, int idade) {
+        super(nome, sobrenome, endereco, email, CPF, RG, telefone, idade);
         this.pago = pago;
         this.diaPagamento = diaPagamento;
         this.mesPagamento = mesPagamento;
